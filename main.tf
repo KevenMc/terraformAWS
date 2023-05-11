491e103c112c1e601d91f3c2358a0b598ec2f71c
keven <keven.mck@gmail.com>
2023-05-11 14:18:14 +0100

    template

---
+++ main.tf
@@ -0,0 +1,98 @@
+resource "aws_vpc" "dev_vpc" {
+  cidr_block           = "10.123.0.0/16"
+  enable_dns_hostnames = true
+
+  tags = {
+    Name = "dev"
+  }
+}
+
+resource "aws_subnet" "dev_public_subnet" {
+  vpc_id                  = aws_vpc.dev_vpc.id
+  cidr_block              = "10.123.1.0/24"
+  map_public_ip_on_launch = true
+  availability_zone       = "eu-west-2a"
+
+  tags = {
+    Name = "dev-public"
+  }
+}
+
+resource "aws_internet_gateway" "dev_internet_gateway" {
+  vpc_id = aws_vpc.dev_vpc.id
+
+  tags = {
+    Name = "dev-igw"
+  }
+}
+
+resource "aws_route_table" "dev_public_rt" {
+  vpc_id = aws_vpc.dev_vpc.id
+
+  tags = {
+    Name = "dev-public-rt"
+  }
+}
+
+resource "aws_route" "default_route" {
+  route_table_id         = aws_route_table.dev_public_rt.id
+  destination_cidr_block = "0.0.0.0/0"
+  gateway_id             = aws_internet_gateway.dev_internet_gateway.id
+}
+
+resource "aws_route_table_association" "dev_public_assoc" {
+  subnet_id      = aws_subnet.dev_public_subnet.id
+  route_table_id = aws_route_table.dev_public_rt.id
+}
+
+
+resource "aws_security_group" "dev_sg" {
+  name        = "dev_sg"
+  description = "dev security group"
+  vpc_id      = aws_vpc.dev_vpc.id
+
+  ingress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["${var.public_ip}/32"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_key_pair" "dev_auth" {
+  key_name   = "dev_key"
+  public_key = file("~/.ssh/dev_key.pub")
+}
+
+resource "aws_instance" "dev_node" {
+  instance_type          = "t2.micro"
+  ami                    = data.aws_ami.server_ami.id
+  key_name               = aws_key_pair.dev_auth.id
+  vpc_security_group_ids = [aws_security_group.dev_sg.id]
+  subnet_id              = aws_subnet.dev_public_subnet.id
+  user_data              = file("userdata.tpl")
+
+  root_block_device {
+    volume_size = 10
+  }
+
+  tags = {
+    Name = "dev-node"
+  }
+
+  provisioner "local-exec" {
+    command = templatefile("ssh-config.tpl", {
+      hostname     = self.public_ip,
+      user         = "ubuntu",
+      identityfile = "~/.ssh/dev_key"
+    })
+    interpreter = ["bash", "-c"]
+  }
+}
